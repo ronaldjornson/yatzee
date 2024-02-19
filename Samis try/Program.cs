c41456968550e8fbe28c47c4b840cfc60d686e42
Sami <seel63284@edu.ucl.dk>
2024-02-19 18:05:39 +0100

    testing branch

---
+++ Samis try/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Samis_try
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Klar til noget Yatzee?!");
+            
+            }
+
+        }
+    }
+}
