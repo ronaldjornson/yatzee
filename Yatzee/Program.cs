523a355f7780d17068bcdbde48397e98aecf5351
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-02-21 09:34:02 +0100

    Forsøg på at fikse netsd fejl

--- Yatzee/Program.cs
+++ Yatzee/Program.cs
@@ -4,31 +4,31 @@
     {
         static void Main(string[] args)
         {
-            
-            
-            
-            //Renés roderi:
-            //Console.WriteLine("Velkommen til Yatzee!");
-
-            //Console.WriteLine("Spiller 1, skriv dit navn og afslut med Enter:");
-            //    string spiller1 = Console.ReadLine();
-
-            //Console.WriteLine("Spiller 2, skriv dit navn og afslut med Enter:");
-            //    string spiller2 = Console.ReadLine();
 
 
-            //Console.WriteLine(spiller1 + " kast terningerne med Enter!");
-            //Console.ReadLine();
-            //    {
-            //    int terning1, terning2, terning3, terning4, terning5;
-            //    Random r = new Random();
-            //    terning1 = r.Next(1, 6);
-            //    terning2 = r.Next(1, 6);
-            //    terning3 = r.Next(1, 6);
-            //    terning4 = r.Next(1, 6);
-            //    terning5 = r.Next(1, 6);
-            //    Console.WriteLine(spiller1 + " du har slået: " + terning1 + ", " + terning2 + ", " + terning3 + ", " + terning4 + " & " + terning5);
-            //    }
+
+            //Renés roderi :
+            Console.WriteLine("Velkommen til Yatzee!");
+
+            Console.WriteLine("Spiller 1, skriv dit navn og afslut med Enter:");
+            string spiller1 = Console.ReadLine();
+
+            Console.WriteLine("Spiller 2, skriv dit navn og afslut med Enter:");
+            string spiller2 = Console.ReadLine();
+
+
+            Console.WriteLine(spiller1 + " kast terningerne med Enter!");
+            Console.ReadLine();
+            {
+                int terning1, terning2, terning3, terning4, terning5;
+                Random r = new Random();
+                terning1 = r.Next(1, 6);
+                terning2 = r.Next(1, 6);
+                terning3 = r.Next(1, 6);
+                terning4 = r.Next(1, 6);
+                terning5 = r.Next(1, 6);
+                Console.WriteLine(spiller1 + " du har slået: " + terning1 + ", " + terning2 + ", " + terning3 + ", " + terning4 + " & " + terning5);
+            }
 
             //if (input < 9)
             //{
@@ -38,7 +38,7 @@
             //         " maximum turns.", input);
             //}
 
-            //Console.ReadLine();
+            Console.ReadLine();
 
         }
     }
